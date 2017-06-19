4a374b970a8f44ab3b8f8a093c2fdf4872668522
Kevin Huang <huangkun@unionpaysmart.com>
2017-06-19 11:12:26 +0800

    init gradle template project

---
+++ src/main/java/com/unionpaysmart/template/application/Application.java
@@ -0,0 +1,35 @@
+package com.unionpaysmart.template.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (C), 2016, 银联智惠信息服务（上海）有限公司
+ * 启动类.
+ *
+ * @author Kevin Huang
+ * @since v0.0.1
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    /**
+     * echo method.
+     */
+    public void echo() {
+        LOGGER.info("Hello World");
+    }
+
+    /**
+     *  Spring Boot 启动方法.
+     * @param args 输入参数
+     */
+    public static void main(final String[] args) {
+        new SpringApplication(Application.class).run(args);
+    }
+}
